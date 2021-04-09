7c5ad0e101ef8cbb5b38fdd29a5ddf1c1680770e
Bharat <bharatdev4342@gmail.com>
2021-04-09 14:02:31 +0530

    Controller spec

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe HomeController, type: :controller do
+  describe 'GET index' do
+    before do
+      expect_any_instance_of(SocialAggregator).to receive(:execute).once.and_return(aggregator_response)
+      get :index
+    end
+
+    context 'when result is success' do
+      let(:result) do
+        {
+          twitter: %w[tweet1 tweet2],
+          facebook: %w[status1 status2],
+          instagram: %w[photo1 photo2]
+        }
+      end
+      let(:aggregator_response) do
+        s = SocialAggregator.new
+        s.result = result
+      end
+
+      it 'returns fetched data' do
+        expect(response.body).to eq result.to_json
+      end
+
+      it 'responds with 200' do
+        expect(response.code).to eq '200'
+      end
+    end
+  end
+end
