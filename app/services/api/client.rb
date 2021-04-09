6e1156aa6cc66a859be957ec6710e48189b985c5
Bharat <bharatdev4342@gmail.com>
2021-04-09 18:19:55 +0530

    Refactor client

---
+++ app/services/api/client.rb
@@ -0,0 +1,14 @@
+module Api
+  class Client
+    API_ENDPOINT = 'https://takehome.io'.freeze
+
+    def initialize(endpoint: API_ENDPOINT)
+      @endpoint = endpoint
+    end
+
+    def call
+      @_client ||= Faraday.new(@endpoint) do |client|
+      end
+    end
+  end
+end
